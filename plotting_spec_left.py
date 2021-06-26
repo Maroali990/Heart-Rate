1b41d28f283a7f05e7259cb8b4e721923cbdc723
Manar A <49908515+Maroali990@users.noreply.github.com>
2021-06-26 15:25:50 +0200

    Add files via upload
    Add 3D plots

---
+++ plotting_spec_left.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+import numpy as np
+plot3d=pd.read_csv("spectogram_leftCheek (4).csv", sep=";")
+
+
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+# Make data.
+X = np.arange(61)
+Y = np.linspace(0.7, 2, 9)
+
+X, Y = np.meshgrid(X, Y)
+#R = np.sqrt(X**2 + Y**2)
+Z = plot3d.T
+
+# Plot the surface.
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+# Customize the z axis.
+#ax.set_zlim(-1.01, 1.01)
+ax.zaxis.set_major_locator(LinearLocator(10))
+# A StrMethodFormatter is used automatically
+ax.zaxis.set_major_formatter('{x:.02f}')
+ax.set_xlabel("times pwelch segments are averaged")
+ax.set_ylabel("freq HZ")
+ax.set_zlabel("magnitude power")
+plt.title("Spectogram leftCheek")
+
+
+
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=5)#
+fig.set_size_inches(10, 10)
+
+
+plt.show()
+
